0ef12828fa84c1b15ad1e2a3c954317d826731a3
Zendeni <53412927+Zendeni@users.noreply.github.com>
2024-05-18 14:43:59 +0200

    Update zip_file_cracker.py

--- zipfilecracker/zip_file_cracker.py
+++ zipfilecracker/zip_file_cracker.py
@@ -1,29 +1,27 @@
 import zipfile
-import optparse
+import argparse
 from threading import Thread
 
 def extractFile(zFile, password):
     try:
-        zFile.extractall(pwd=password)
-        print('[+] Found Password ' + password + '\n')
+        zFile.extractall(pwd=password.encode('utf-8'))
+        print('[+] Found Password: ' + password + '\n')
     except:
         pass
+
 def main():
-    parser = optparse.OptionParser("usage="%prog "+\" -f <zipfile> -d <dictionary>")
-    parser.add_option('-f', dest='zFile', type='string', help='specify zip file')
-    parser.add_option('-d', dest='dFile', type='string', help='specify dictionary file')
-    (options, args) = parser.parse_args()
-    if (options.zFile == None) | (options.dFile == None):
-        print(parser.usage)
-        exit(0)
-    else:
-        zname = options.zFile
-        dname = options.dFile
-    zFile = zipfile.ZipFile(zname)
-    passFile = open(dname)
+    parser = argparse.ArgumentParser(description='Zip file password cracker using a dictionary attack')
+    parser.add_argument('-f', dest='zFile', type=str, required=True, help='specify zip file')
+    parser.add_argument('-d', dest='dFile', type=str, required=True, help='specify dictionary file')
+    args = parser.parse_args()
+
+    zFile = zipfile.ZipFile(args.zFile)
+    passFile = open(args.dFile, 'r')
+
     for line in passFile.readlines():
-        password = line.strip('\n')
+        password = line.strip()
         t = Thread(target=extractFile, args=(zFile, password))
         t.start()
 
 if __name__ == '__main__':
+    main()
